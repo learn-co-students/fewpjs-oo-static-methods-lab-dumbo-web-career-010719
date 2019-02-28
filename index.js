e5b5d180bb1635f8de5d46d9dba91bfae5870be4
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2019-02-28 09:35:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
   //add static methods here
+  static capitalize(str){
+    return str.charAt(0).toUpperCase()+str.slice(1)
+  }
+
+  static sanitize(str){
+    return str.replace(/[^s, A-Za-z0-9-']+/g, '').trim();
+  }
+
+  static titleize(str){
+    let wordsArray =["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    const arrayStr=str.split(" ")
+    const newArray=[]
+    arrayStr.forEach((str, index) =>{
+      if(index === 0){
+        newArray.push(this.capitalize(str))
+      }
+      else if(wordsArray.includes(str)){
+        newArray.push(str)
+      }else{
+        newArray.push(this.capitalize(str))
+      }
+    })  
+    return newArray.join(" ") 
+  }
+  
 }
